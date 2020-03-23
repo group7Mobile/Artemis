fda027c1353045ab5afefd1ce5470e44c284341b
jsw19 <49043747+jsw19@users.noreply.github.com>
2020-03-23 22:12:49 +0000

    history checking

--- app/src/main/java/com/example/artemis/Privacy.java
+++ app/src/main/java/com/example/artemis/Privacy.java
@@ -3,19 +3,54 @@ package com.example.artemis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.Browser;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Observable;
 
 public class Privacy extends AppCompatActivity {
+
+    private HistoryDBHelper dbHelper;
+    private ArrayList<String> arrayList;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy);
+        dbHelper = new HistoryDBHelper(this);
+        listView =  findViewById(R.id.listviewH);
+        arrayList = new ArrayList<>();
+        getBrowserHist();
+
+        //constructor of adapter to store input item separately in list_item and put them in list_view
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.txtitem, arrayList);
     }
 
     public void mainPage(View v) {
         Intent goMainPage = new Intent(this, Settings.class);
         startActivity(goMainPage);
     }
+    public void getBrowserHist()  {
+
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+            String selectString = "SELECT * FROM history_table";
+            Cursor cursor = db.rawQuery(selectString, null);
+            if (cursor.moveToFirst()) {
+                do {
+                    arrayList.add(cursor.getString(cursor.getColumnIndex("URL")));
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
+            db.close();
+        }
+
 }
