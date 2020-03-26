96c5c09dc583f7b7254e3662fd25bd498483f6d1
jamie <rjb19179@uni.strath.ac.uk>
2020-03-26 16:21:41 +0000

    Implemented changing background colour, and fixed history database.

--- app/src/main/java/com/example/artemis/Background.java
+++ app/src/main/java/com/example/artemis/Background.java
@@ -1,12 +1,15 @@
 package com.example.artemis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Background extends AppCompatActivity {
     private Button a;
@@ -31,5 +34,59 @@ public class Background extends AppCompatActivity {
     public void mainPage(View v) {
         Intent goMainPage = new Intent(Background.this, Settings.class);
         startActivity(goMainPage);
+    }
+
+    public void changeA(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("bg", 0);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("bg", R.color.colorBG1);
+        editor.commit();
+        showToast();
+    }
+
+    public void changeB(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("bg", 0);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("bg", R.color.colorBG2);
+        editor.commit();
+        showToast();
+    }
+
+    public void changeC(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("bg", 0);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("bg", R.color.colorBG3);
+        editor.commit();
+        showToast();
+    }
+
+    public void changeD(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("bg", 0);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("bg", R.color.colorBG4);
+        editor.commit();
+        showToast();
+    }
+
+    public void changeE(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("bg", 0);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("bg", R.color.colorBG5);
+        editor.commit();
+        showToast();
+    }
+
+    public void changeF(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("bg", 0);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("bg", R.color.colorBG6);
+        editor.commit();
+        showToast();
+    }
+
+    public void showToast() {
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, "Background colour changed", duration);
+        toast.show();
     }
 }
