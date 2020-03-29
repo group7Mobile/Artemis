de9c1d9787d82f5edf13e2c4506f3504a6c061d9
jamie <rjb19179@uni.strath.ac.uk>
2020-03-29 18:41:48 +0100

    Fixed blacklist database reference in getBlockedSites.

--- app/src/main/java/com/example/artemis/MainActivity.java
+++ app/src/main/java/com/example/artemis/MainActivity.java
@@ -467,7 +467,7 @@ public class MainActivity extends AppCompatActivity {
         if (cursor.moveToFirst()) {
             do {
                 blockedList.add(filterBlocked(cursor
-                        .getString(cursor.getColumnIndex("URL"))));
+                        .getString(cursor.getColumnIndex("Url"))));
             } while (cursor.moveToNext());
         }
         cursor.close();
