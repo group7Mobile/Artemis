21ab37d23218abb16bb99335600cc49626d395a2
jsw19 <49043747+jsw19@users.noreply.github.com>
2020-03-30 09:04:07 +0100

    before submission code checking

--- app/src/main/java/com/example/artemis/Filters.java
+++ app/src/main/java/com/example/artemis/Filters.java
@@ -14,8 +14,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -25,12 +27,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Filters extends AppCompatActivity {
-    private FilterAdapter filterAdapter;
+    private RecyclerAdapter recyclerAdapter;
     private EditText txtInput;
     private SharedPreferences fav;
     private FilterWordsDBhelper filterWords;
     public ArrayList<String> arrayList;
     String wordId= null;
+    public boolean status;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,8 +43,8 @@ public class Filters extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recyclerView2);
         arrayList = new ArrayList<>();
         arrayList = getFilterWords();
-        filterAdapter = new FilterAdapter(this, arrayList);
-        recyclerView.setAdapter(filterAdapter);
+        recyclerAdapter = new RecyclerAdapter(this, arrayList);
+        recyclerView.setAdapter(recyclerAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         //constructor of adapter to store input item separately in list_item and put them in list_view
         Button btnAdd = (Button) findViewById(R.id.button2);
@@ -50,9 +53,10 @@ public class Filters extends AppCompatActivity {
             public void onClick(View v) {
                 txtInput = (EditText) findViewById(R.id.editText18);
                 String newItem = txtInput.getText().toString();
+
                 //every time add an item, add it in the top of stack by adding it to the 0 index of the arrayList
                 addTofilterDB(newItem);
-                filterAdapter.notifyDataSetChanged();
+                recyclerAdapter.notifyDataSetChanged();
             }
         });
 
@@ -64,7 +68,9 @@ public class Filters extends AppCompatActivity {
     public ArrayList<String> getArrayList(){
         return arrayList;
     }
-
+    public boolean getStatus(){
+        return status;
+    }
     public void addTofilterDB(String word) {
         SQLiteDatabase db = filterWords.getWritableDatabase();
         ContentValues values = new ContentValues();
